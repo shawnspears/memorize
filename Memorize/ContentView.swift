3928e1769cb1a720737b68d386955f1701bba720
shawnspears <shwnspears@gmail.com>
2022-03-07 01:03:05 -0800

    Add dark mode contentview preview

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -19,17 +19,25 @@ struct ContentView: View {
         .foregroundColor(.red)
     }
 }
+
 struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
                 .stroke(lineWidth: 3)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 20)
+                .fill().foregroundColor(.white)
+            Text("✈️")
+                .font(.largeTitle)
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
